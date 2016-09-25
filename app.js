b4025f9870397ffc12962dceee975271f551ae8a
davminguez <dave.defeat@gmail.com>
2016-09-24 21:25:33 -0600

    added function+style

--- app.js
+++ app.js
@@ -1,11 +1,29 @@
-//Do Not Modify the getMusic function
-function getMusic(){
+//Do Not Modify the getMusic f unction
+function getMusic() {
   var artist = document.getElementById('artist').value;
   itunes.getMusicByArtist(artist).then(drawSongs);
 }
 
 
 
-function drawSongs(songList){
+function drawSongs(songList) {
+
+  let itemList = $('#song-list');
+  let art = $('#album-art');
+  // clears out previous search results
+  itemList.empty();
   console.log(songList);
+
+  songList.forEach(function (song) {
+    let songPrev = song.preview;
+
+    let card = '<img width="75px" src="' + song.albumArt + '"><ul><li id="songTitle"><a href="' + songPrev +
+      '"><i class="fa fa-play-circle-o fa-2x" aria-hidden="true"></i><audio controls><source src="' + songPrev + '"></audio></a> ' + song.title + '</li><li id="priceText"><strong>' + "Price</strong> / $"
+      + song.price + '</li><li><strong>' + "By</strong> / " + song.artist + '</li><li><strong>' + "Album </strong> / " + song.collection + '</li></ul>';
+
+    itemList.append(card);
+
+  });
+
+
 }
